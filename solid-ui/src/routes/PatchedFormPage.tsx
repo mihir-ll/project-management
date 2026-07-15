1c6d72f6b9f6bc0eb22a048a3eec74393deaac98
Mihir <mihir.thakur@solidxai.com>
2026-07-15 16:02:39 +0530

    task form page

--- solid-ui/src/routes/PatchedFormPage.tsx
+++ solid-ui/src/routes/PatchedFormPage.tsx
@@ -1,5 +1,17 @@
 import { AdminFormPage } from "@solidxai/core-ui";
+import { camelCase } from "lodash";
+import { useParams } from "react-router-dom";
+
+import { ProjectTaskFormPage } from "../project-management/admin-layout/project-task/ProjectTaskFormPage";
 
 export function PatchedFormPage() {
+  const params = useParams();
+  const moduleName = params.moduleName || "";
+  const modelName = params.modelName ? camelCase(params.modelName) : "";
+
+  if (moduleName === "project-management" && modelName === "projectTask") {
+    return <ProjectTaskFormPage />;
+  }
+
   return <AdminFormPage />;
 }
